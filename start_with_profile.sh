c394d5e3290536a427f61161a1ef24abb81d7239
Rohan Bhattacharya <rohanb1985@gmail.com>
2019-09-07 19:32:26 +0530

    Support to run customized Java profile

--- start_with_profile.sh
+++ start_with_profile.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#
 # Start SonarQube
 ./bin/run.sh &
 
 LANGUAGE=java
-PROFILE_NAME=StandardProfile
+PROFILE_NAME=SonarWayUpdated
 BASE_URL=http://127.0.0.1:9000
 
 function isUp {
@@ -20,7 +19,7 @@ do
 done
 
 # Restore qualityprofile
-curl -v -u admin:admin -F "backup=@/qualityprofile/java-standardprofile.xml" -X POST "$BASE_URL/api/qualityprofiles/restore"
+curl -v -u admin:admin -F "backup=@/qualityprofile/SonarWayUpdated.xml" -X POST "$BASE_URL/api/qualityprofiles/restore"
 curl -v -u admin:admin -X POST "$BASE_URL/api/qualityprofiles/set_default?language=$LANGUAGE&profileName=$PROFILE_NAME"
 
 wait
